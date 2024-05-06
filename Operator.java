511bd5ad550d43046383b118c647f5c179451821
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2024-05-06 11:41:48 +0800

    feat: Add Operator class with arithmetic, assignment, comparison, and logical operators

---
+++ Operator.java
@@ -0,0 +1,26 @@
+public class Operator {
+    public static void main(String[] args) {
+        //算數運算子
+        /*
+        int x = 3+4;
+        System.out.println("3+4 = " + x);
+        */
+        // 指定運算子、單元運算子
+        /*int x = 5;
+        x ++;
+        System.out.println("x++ = " + x);
+        */
+        // 比較運算子
+        /* 
+        boolean x = 3 > 5;
+        System.out.println("3 > 5 = " + x);
+        boolean z = "Hello" == "Hello";
+        System.out.println("Hello == Hello = " + z);
+        */
+        // 邏輯運算子
+        boolean x = true && true;
+        System.out.println("true && true = " + x);
+        boolean y = false || true;
+        System.out.println("false || true = " + y);
+    }
+}
