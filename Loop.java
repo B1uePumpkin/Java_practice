0c9ecf2b886375a2a4f3c537f8c73edef9b77caa
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2024-05-06 19:40:02 +0800

    feat: Add Loop class with different types of loops

---
+++ Loop.java
@@ -0,0 +1,22 @@
+public class Loop {
+    public static void main(String[] args) {
+        //while 迴圈
+        /*int x = 2;
+        while (x < 5) {
+            System.out.println("x = " + x);
+            x++;
+        }
+        */
+        //do while 迴圈
+        /*int x = 2;
+        do {
+            System.out.println("x = " + x);
+            x++;
+        } while (x < 5);
+        */
+        //for 迴圈
+        for (int x = 2; x <= 6; x+=2) {
+            System.out.println("x = " + x);
+        }
+    }
+}
