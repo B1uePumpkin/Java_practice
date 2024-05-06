365bcd4f809bf7abd1589cd7a8544dfc92591911
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2024-05-06 11:16:19 +0800

    Add DataTypeVariable class with different data types and print statements

---
+++ DataTypeVariable.java
@@ -0,0 +1,13 @@
+public class DataTypeVariable {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x);
+        long y = 20000000000000000L;
+        System.out.println(y);
+        System.out.println(3.14);
+        System.out.println(3.141592653579F);
+        System.out.println(false);
+        System.out.println('A');
+        System.out.println("Hello, World!");
+    }
+}
